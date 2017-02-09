d74cbfbbe433a1d9504a12f79a90cd33efd42b39
yanglei <2851826749@qq.com>
2017-02-09 13:46:40 +0800

    add problem

---
+++ problemGo/24.go
@@ -0,0 +1,66 @@
+package main
+
+import (
+    "fmt"
+)
+
+type ListNode struct {
+    Val int
+    Next *ListNode
+}
+
+func main() {
+    var arr = []int{}
+    var head *ListNode
+    var last *ListNode
+
+    for _, x := range arr{
+        var tp * ListNode
+        tp = new(ListNode)
+        tp.Val = x
+        tp.Next = nil
+        if last != nil {
+            last.Next = tp
+        }
+        if head == nil {
+            head = tp
+        }
+        last = tp
+    }
+    //showList(head)
+    swapPairs(head)
+}
+
+func swapPairs(head *ListNode) *ListNode{
+    var new_head *ListNode
+    var last *ListNode
+    if head == nil || head.Next == nil{
+        return head
+    }
+    left := head
+    right := head.Next
+    new_head = right
+    for left != nil && right != nil {
+        if last != nil {
+            last.Next = right
+        }
+        left.Next = right.Next
+        right.Next = left
+        last = left
+        left = left.Next
+        if left == nil {
+            break
+        } else {
+            right = left.Next
+        }
+    }
+    return new_head
+}
+
+
+func showList(head *ListNode){
+    for head != nil {
+        fmt.Println(head.Val)
+        head = head.Next
+    }
+}
---
+++ problemGo/28.go
@@ -0,0 +1,58 @@
+package main
+
+import "fmt"
+
+func main() {
+    s1 := "ababaab"
+    s2 := ""
+    res := strStr(s1, s2)
+    fmt.Println(res)
+}
+
+func strStr(haystack string, needle string) int {
+    n := len(haystack)
+    m := len(needle)
+    if n == 0 && m > 0 {
+        return -1
+    }
+    if n == 0 || m == 0 {
+        return 0
+    }
+    f := getFail(needle)
+    //fmt.Println(f)
+    j := 0
+    for i := 0; i < n; i++ {
+        for j > 0 && needle[j] != haystack[i] {
+            j = f[j]
+        }
+        if needle[j] == haystack[i] {
+            j++
+        }
+        if j == m {
+            return i-m+1
+        }
+    }
+    return -1
+}
+
+func getFail(needle string) []int {
+    //m = len([]rune(needle))
+    var f = []int{}
+    for i := 0; i < len(needle)+2; i++{
+        f = append(f, 0)
+    }
+    f[0] = 0
+    f[1] = 0
+    for i := 1; i < len(needle); i++ {
+        j := f[i]
+        for j > 0 && needle[i] != needle[j] {
+            j = f[j]
+        }
+        if needle[i] == needle[j] {
+            f[i+1] = j+1
+        } else {
+            f[i+1] = 0
+        }
+    }
+    return f
+}
