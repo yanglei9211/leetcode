d540b8d4e753d15bc62279d7dfa2fd3d15453a4d
yanglei <2851826749@qq.com>
2017-02-10 19:12:48 +0800

    update

--- problemGo/48.go
+++ problemGo/48.go
@@ -1,16 +1,39 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+    a := [][]int{{1,2,3}, {4,5,6}, {7,8,9}}
+    rotate(a)
 }
 
 func rotate(matrix [][]int)  {
-
+    n := len(matrix)
+    tmp := [][]int{}
+    for i := 0; i < n; i++ {
+        col := []int{}
+        for j := n-1; j >=0; j--{
+            col = append(col, matrix[j][i])
+        }
+        tmp = append(tmp, col)
+    }
+    copy(matrix, tmp)
 }
+
 /*
 1 2
 3 4
 
 3 1
 4 2
+
+1 2 3
+4 5 6
+7 8 9
+
+7 4 1
+8 5 2
+9 6 3
+
+
  */
---
+++ problemGo/49.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func main() {
+    s := []string{"eat", "tea", "tan", "ate", "nat", "bat"}
+    hashStr(s[0])
+}
+
+func groupAnagrams(strs []string) [][]string {
+    ans := [][]string{}
+    return ans
+}
+
+func hashStr(s string) int {
+    val := []byte(s)
+    fmt.Println(val)
+    return 0
+}
