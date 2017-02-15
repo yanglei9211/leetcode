5a51de11698c33cfa77878437d5478aa12d36545
yanglei <2851826749@qq.com>
2017-02-15 18:07:16 +0800

    update

--- problemGo/10.go
+++ problemGo/10.go
@@ -5,30 +5,48 @@ import "fmt"
 func main() {
     s := "aab"
     p := "c*a*b"
-    var save byte
-    save = p[0]
-    for i := 0; i < len(p); i++ {
-        if sa
-
-    }
-    fmt.Println(save)
-    fmt.Println(s, p)
+    fmt.Println(isMatch(s, p))
 }
 
 func isMatch(s string, p string) bool {
-    return false
-}
-
-func tryMath(s, p string, x, y int) bool {
-    // var save byte
-    if p[y] >= 'a' && p[y] <= 'z'{
-        if s[x] != p[y]{
-            return false
-        } else {
-            for x < len(s) && s[x] == p[y] && y < len(p) - 1 && p[y+1] == '*' {
-                x++
-            }
+    for i := 0; i < len(p); i++ {
+        fmt.Println(i)
+        if p[i] != '*' && tryMatch(s, p, 0, i) {
+            return true
         }
     }
     return false
+}
+
+func tryMatch(s, p string, x, y int) bool {
+    var save byte
+    for x < len(p) && y < len(p){
+        if p[y] >= 'a' && p[y] <= 'z'{
+            if s[x] != p[y]{
+                return false
+            } else {
+                if y < len(s) && s[x] == save {
+                    for x < len(s) && s[x] == save {
+                        x++
+                    }
+                    y++
+                }
+                y++
+            }
+        } else if p[y] == '.' {
+            save := s[x]
+            if y < len(p) - 1 && p[y+1] == '*'{
+                for x < len(s) && s[x] == save{
+                    x++
+                }
+                y++
+            }
+            y++
+        }
+    }
+    if x == len(s) {
+        return true
+    } else {
+        return false
+    }
 }
